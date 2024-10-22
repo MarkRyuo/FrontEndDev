9e95d3323eb16f5d0fcb63669ecc669bc4fca3d5
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-22 08:18:55 +0800

    Update: System

--- PlayGround/Play1/script.js
+++ PlayGround/Play1/script.js
@@ -2,5 +2,5 @@
 
 document.getElementById("nav").innerHTML = "<p>NavBar</p>" ;
 document.getElementById("aside").innerHTML = "<p>Aside</p>" ;
-document.getElementById("main").innerHTML = "<p>Main</p>" ;
+document.getElementById("main").innerHTML += "<p>Main</p>" ;
 document.getElementById("footer").innerHTML = "<p>Footer</p>" ;
