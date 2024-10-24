6521e91ce497873ea5c4b9dd1422c3898de7b697
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-24 09:27:25 +0800

    Update: System

--- PlayGround/Play2/script.js
+++ PlayGround/Play2/script.js
@@ -8,3 +8,11 @@ const handleShow = () => {
     document.getElementById("article").innerHTML = "<p>Article</p>";
     document.getElementById("footer").innerHTML = "<p>Footer</p>";
 }
+
+
+document.getElementById("header").innerHTML = "<p>Header</p>";
+document.getElementById("nav").innerHTML = "<p>Nav</p>";
+document.getElementById("section").innerHTML = "<p>Section</p>";
+document.getElementById("aside").innerHTML = "<p>Aside</p>";
+document.getElementById("article").innerHTML = "<p>Article</p>";
+document.getElementById("footer").innerHTML = "<p>Footer</p>";
