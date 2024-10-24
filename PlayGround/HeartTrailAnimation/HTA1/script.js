534faa06cc6c35df9af63f78b45b7dffd2a4d389
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-25 07:54:39 +0800

    Subject: Body

--- PlayGround/HeartTrailAnimation/HTA1/script.js
+++ PlayGround/HeartTrailAnimation/HTA1/script.js
@@ -1,6 +1,6 @@
 const bodyEl = document.querySelector("body") ;
 
 bodyEl.addEventListener("mousemove", (event) => {
-    const xPos = event.offsetX
+    const xPos = event.offsetX ;
     const yPos = event.offsetY
 })
