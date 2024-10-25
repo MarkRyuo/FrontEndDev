97c1527f617deb798f99c7369974c73a203b768d
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-26 07:07:39 +0800

    Update: System

--- PlayGround/HeartTrailAnimation/HTA2/script.js
+++ PlayGround/HeartTrailAnimation/HTA2/script.js
@@ -5,7 +5,7 @@ mainEl.addEventListener("mousemove", (event) => {
     const posX = event.offsetX ; //* Declare pos x 
     const posY = event.offsetY ; //* Declare pos y 
     const spanEl = document.createElement("span"); //* Create Element <span></span>
-    mainEl.appendChild("spanEl"); //* Append to the main element the newly created span
+    mainEl.appendChild(spanEl); //* Append to the main element the newly created span
 
     
     spanEl.style.top = posX + 'px' ; //* design the posX to the top
