f34c5ef1686378d9d7317e88482445975996db95
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-26 06:50:28 +0800

    Update: System

--- PlayGround/HeartTrailAnimation/HTA2/script.js
+++ PlayGround/HeartTrailAnimation/HTA2/script.js
@@ -3,9 +3,10 @@ const mainEl = document.addEventListener("main") ;
 mainEl.document.addEventListener("mousemove", (event) => {
     const posX = event.offsetX ;
     const posY = event.offsetY ;
-    const spanEL = document.createElement("span")
+    const spanEl = document.createElement("span")
     mainEl.appendChild("spanEl")
 
     spanEl.style.top = posX + 'px' ;
-    spanEl.style.left = posY + 'px'
+    spanEl.style.left = posY + 'px' ;
+
 })
