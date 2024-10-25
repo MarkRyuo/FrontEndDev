c474314b83c4836bd6585e8201df8c9cdfb5fe58
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-26 06:56:59 +0800

    Update: System

--- PlayGround/HeartTrailAnimation/HTA2/script.js
+++ PlayGround/HeartTrailAnimation/HTA2/script.js
@@ -6,10 +6,10 @@ mainEl.document.addEventListener("mousemove", (event) => {
     const spanEl = document.createElement("span"); //* Create Element <span></span>
     mainEl.appendChild("spanEl") //* Append to the main element the newly created span
 
-    const size = Math.random() * 100 ; //* Add a random number to size the newly created span
-
+    
     spanEl.style.top = posX + 'px' ; //* design the posX to the top
     spanEl.style.left = posY + 'px' ; //* design the posY to the left
-
+    const size = Math.random() * 100 ; //* Add a random number to size the newly created span
+    spanEl.style.width = size + 
 
 })
